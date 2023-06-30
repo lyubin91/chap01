46e008b3f618b1f20596cc95b94d7cb0afc4f5f8
lyubin <lyubin@example.com>
2023-06-30 18:15:25 +0900

    수정수정수정

--- Triangle.java
+++ Triangle.java
@@ -4,6 +4,7 @@ class Triangle {
 	public static void main(String[] args) {
 	System.out.println("    *");	
 	System.out.println("   **");	
+	System.out.println("   **");	
 	System.out.println("  ***");	
 	System.out.println(" ****");	
 	System.out.println("*****");	
